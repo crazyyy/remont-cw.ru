409255e096e671c63a554cf3441e93bfd0b3f664
crazyyy <crazyyy@gmail.com>
2015-09-14 17:09:43 +0300

    some cleaners

--- wordpress/wp-content/plugins/wordfence/tmp/configCache.php
+++
@@ -1,4 +0,0 @@
-<?php
-/* Wordfence temporary file security header */
-echo "Nothing to see here!\n"; exit(0);
-?>a:10:{s:9:"cacheType";s:0:"";s:15:"firewallEnabled";s:1:"1";s:6:"apiKey";s:128:"6ffe5fcb49b195366a103d1f181d2c3d12ad2eb9ff270b4714c05d51cd607a4858944505b1510181dccff33179b92632e84300798e677fe51af355d3d205f4cb";s:9:"howGetIPs";s:0:"";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:10:"tourClosed";s:0:"";s:17:"actUpdateInterval";s:0:"";s:7:"debugOn";s:1:"0";s:13:"welcomeClosed";s:1:"1";}
--- wordpress/wp-content/themes/Orion/Orion/bottom.php
+++
@@ -1,9 +0,0 @@
-<div id="bottom">
-	<ul>
-		<?php if ( !function_exists('dynamic_sidebar')
-			|| !dynamic_sidebar("Footer") ) : ?>  
-		<?php endif; ?>
-	</ul>
-	<div class="clear"> </div>
-</div>
-	
