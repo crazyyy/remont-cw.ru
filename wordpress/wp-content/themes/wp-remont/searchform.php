5738dbe07167943f3eaf22c0039e8873a0438606
Vitalii Antoniuk <va@jelasric.com>
2018-12-17 00:26:20 +0200

    close and archive project

--- wordpress/wp-content/themes/wp-remont/searchform.php
+++
@@ -1,4 +0,0 @@
-<form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
-  <input class="search-input" type="search" name="s" placeholder="|">
-  <button class="search-submit fa fa-search" type="submit" role="button"></button>
-</form><!-- /search -->
